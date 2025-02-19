981bda7241292a412cd70ca36967f133d3d08c77
xingjichu <QKJ15166659769@outlook.com>
2025-02-19 12:29:32 +0800

    2023202309+function3_1

--- 2023202309.c
+++ 2023202309.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
+#include <limits.h>
 int main()
 {
     printf("This is the first lab!\\n");
     printf("This is the second lab!\n");
+    printf("Sum: %d\n", sum);
     return 0;
 }
